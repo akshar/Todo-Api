ab2988cbb0d4c4c9c95df05da5599f32148cfb04
Akshar Dave <akshar314@gmail.com>
2016-04-04 15:34:06 +0530

    Add POST for todos

--- server.js
+++ server.js
@@ -1,52 +1,55 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-        id: 1,
-        description: "Get the papers from Capg",
-        completed: false
+var todos = [];
+var todoNextId = 1;
 
-    }, {
-        id: 2,
-        description: "get a beer from madhuloka",
-        completed: false
-
-    }, {
-        id: 3,
-        description: "Cook now!",
-        completed: true
-
-    }
-
-];
-
-
+app.use(bodyParser.json());
 app.get('/', function(request, response) {
     response.send('TODO API');
 
 });
 
+//GET /todos
 app.get('/todos', function(request, response) {
 
     response.json(todos);
 
 });
 
+//GET /todos/:id
 app.get('/todos/:id', function(request, response) {
-	var todoId = parseInt(request.params.id,10);  // req.param returns string we need to convert it to Int
-	var matchedTodo;
-	todos.forEach(function(todo){
-		if(todoId === todo.id){
-			matchedTodo = todo;
-		}
+    var todoId = parseInt(request.params.id, 10); // req.param returns string we need to convert it to Int
+    var matchedTodo;
+    todos.forEach(function(todo) {
+        if (todoId === todo.id) {
+            matchedTodo = todo;
+        }
 
-	});
+    });
     // response.send("Requested id by the user is " + request.params.id);
-    if(matchedTodo){
-    response.json(matchedTodo);
-    }else{
-    	response.status(404).send();
+    if (matchedTodo) {
+        response.json(matchedTodo);
+    } else {
+        response.status(404).send();
     }
+
+});
+
+// Post /todos
+
+app.post('/todos', function(request, response) {
+
+    var body = request.body;
+    body.id = todoNextId;
+
+    todoNextId ++;
+    todos.push(body);
+
+
+        // console.log("description"+request.body.description);
+        response.json(body);
 
 });
 
