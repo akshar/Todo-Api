6f59298ceadd7b59ca22be0f50c7bebe6e8f5092
Akshar Dave <akshar314@gmail.com>
2016-04-07 23:10:38 +0530

    final static todos

--- server.js
+++ server.js
@@ -22,6 +22,16 @@ app.get('/todos', function(request, response) {
         filteredTodos = _.where(filteredTodos, { completed: false });
     }
 
+    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+
+        filteredTodos = _.filter(filteredTodos, function(todo) {
+
+            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+
+        });
+    }
+
+
     response.json(filteredTodos);
 
 });
