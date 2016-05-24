731c13779c97f69ecce747b4d1653099f572de29
Akshar Dave <akshar314@gmail.com>
2016-05-24 18:02:26 +0530

    change dialect AGAIN

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 if (env === 'production') {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
-        'dialect': 'postgres'
+        dialect: 'postgres'
     });
 } else {
     sequelize = new Sequelize(undefined, undefined, undefined, {
