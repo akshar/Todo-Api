0e15ef4e8fa1c1a6b258dd3728573201e4edcbc7
Akshar Dave <akshar314@gmail.com>
2016-05-24 17:58:08 +0530

    change dialect

--- db.js
+++ db.js
@@ -3,10 +3,10 @@ var env = process.env.NODE_ENV || 'development';
 var sequelize;
 if (env === 'production') {
     sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: 'postgres'
+        'dialect': 'postgres'
     });
 } else {
-     sequelize = new Sequelize(undefined, undefined, undefined, {
+    sequelize = new Sequelize(undefined, undefined, undefined, {
         'dialect': 'sqlite',
         'storage': __dirname + '/data/dev-todo-api.sqlite'
     });
